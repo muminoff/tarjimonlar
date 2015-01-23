619722ca90beecaa0b5d682802b7ad0c8366f5ed
muminoff <smuminov@gmail.com>
2015-01-24 00:41:31 +0900

    Model fields

--- tarjimonlar/core/models.py
+++ tarjimonlar/core/models.py
@@ -2,18 +2,6 @@ from django.db import models
 
 
 class GroupUser(models.Model):
-    id = models.CharField(primary_key=True)
-    name = models.CharField()
-    # email = models.EmailField()
+    id = models.CharField(max_length=16, primary_key=True)
+    name = models.CharField(max_length=100)
 
-
-# class Post(models.Model):
-#     id = models.CharField(primary_key=True)
-#     created_time = models.DateTimeField()
-#     updated_time = models.DateTimeField()
-#     from_name = models.ForeignKey(GroupUser)
-
-
-
-# class Comment(models.Model):
-#     pass
