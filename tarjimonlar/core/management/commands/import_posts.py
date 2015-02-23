2d2eb7710ec4ba247461c604627c944ecf93ebae
muminoff <smuminov@gmail.com>
2015-02-24 00:55:54 +0900

    Gevent pool

--- tarjimonlar/core/management/commands/import_posts.py
+++ tarjimonlar/core/management/commands/import_posts.py
@@ -3,12 +3,14 @@ from django.conf import settings
 from facepy import GraphAPI
 from core.models import Member, Post
 from getenv import env
+from gevent.pool import Pool
 import sys
 import sys
 
 
 class Command(BaseCommand):
     help = 'Imports posts from Facebook'
+
 
     def get_likes_of_post(self, postid):
         access_token = env('FACEBOOK_ACCESS_TOKEN')
@@ -21,62 +23,72 @@ class Command(BaseCommand):
         return len(likes_arr['data'])
 
 
+    def get_posts(self, feeddata):
+
+        for post in feeddata:
+
+            postid = post['id']
+            postmsg = post['message'] if 'message' in post else ''
+            postctime = post['created_time']
+            postutime = post['updated_time']
+            creatorid = post['from']['id']
+            creatorname = post['from']['name']
+
+            if grab_likes:
+                howmanylikes = self.get_likes_of_post(postid)
+            else:
+                howmanylikes = 0
+
+            post_exists = Post.objects.filter(id=postid).exists()
+            member_exists = Member.objects.filter(id=creatorid).exists()
+
+            if not post_exists:
+                if not member_exists:
+                    creator = Member.objects.create(
+                        pk=creatorid,
+                        name=creatorname
+                    )
+                else:
+                    creator = Member.objects.get(id=creatorid)
+
+                try:
+                    Post.objects.create(
+                        pk=postid,
+                        message=postmsg,
+                        created_time=postctime,
+                        updated_time=postutime,
+                        creator=creator,
+                        likes=howmanylikes
+                    )
+                    new_posts += 1
+                except:
+                    new_posts -= 1
+
+            else:
+                Post.objects.filter(id=postid).update(likes=howmanylikes)
+
+
     def handle(self, *args, **options):
         access_token = env('FACEBOOK_ACCESS_TOKEN')
         grab_likes = env('GRAB_LIKES')
         graph = GraphAPI(access_token)
         group_id = '438868872860349'
         feed = graph.get('{}/feed?limit=1000'.format(group_id))
+        pool = Pool(512)
 
         new_posts = 0
         while 'data' in feed and feed['data'] and \
               len(feed['data']) > 0:
-            for post in feed['data']:
 
-                postid = post['id']
-                postmsg = post['message'] if 'message' in post else ''
-                postctime = post['created_time']
-                postutime = post['updated_time']
-                creatorid = post['from']['id']
-                creatorname = post['from']['name']
 
-                if grab_likes:
-                    howmanylikes = self.get_likes_of_post(postid)
-                else:
-                    howmanylikes = 0
-
-                post_exists = Post.objects.filter(id=postid).exists()
-                member_exists = Member.objects.filter(id=creatorid).exists()
-
-                if not post_exists:
-                    if not member_exists:
-                        creator = Member.objects.create(
-                            pk=creatorid,
-                            name=creatorname
-                        )
-                    else:
-                        creator = Member.objects.get(id=creatorid)
-
-                    try:
-                        Post.objects.create(
-                            pk=postid,
-                            message=postmsg,
-                            created_time=postctime,
-                            updated_time=postutime,
-                            creator=creator,
-                            likes=howmanylikes
-                        )
-                        new_posts += 1
-                    except:
-                        new_posts -= 1
-
-                else:
-                    Post.objects.filter(id=postid).update(likes=howmanylikes)
+            pool.spawn(self.get_posts, feed['data'])
 
 
             newUrl = feed['paging']['next'].replace(
                 'https://graph.facebook.com/', ''
             )
             feed = graph.get(newUrl)
+
+        pool.join()
 
         print 'Total {0} posts added.'.format(new_posts)
