6f32aeeee364531767b7171bbb16bc068621a03e
Sardor <sardor@hanbiro.com>
2015-02-23 22:28:30 +0900

    Count

--- tarjimonlar/core/views.py
+++ tarjimonlar/core/views.py
@@ -11,8 +11,8 @@ def index_page(request):
 def members_page(request):
     top_posters = Member.objects.annotate(num_posts=Count('post')).order_by('-num_posts')[:10]
     top_commentors = Member.objects.annotate(num_comments=Count('comment')).order_by('-num_comments')[:10]
-    top_liked_posters = Post.objects.annotate(creator_times=Sum('creator__name', distinct=True)).order_by('-likes')[:10]
-    top_liked_commentors = Comment.objects.annotate(creator_times=Sum('creator__name', distinct=True)).order_by('-likes')[:10]
+    top_liked_posters = Post.objects.annotate(creator_times=Count('creator__name', distinct=True)).order_by('-likes')[:10]
+    top_liked_commentors = Comment.objects.annotate(creator_times=Count('creator__name', distinct=True)).order_by('-likes')[:10]
     context = {
         "top_posters": top_posters,
         "top_commentors": top_commentors,
