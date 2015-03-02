26b6637fd776b5610a9af92f41d7f45664b124f1
Sardor <sardor@hanbiro.com>
2015-03-02 16:57:14 +0900

    Apply changes script

---
+++ apply.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+rm static/js/comment_chart.js
+rm static/js/posts_chart.js
+
+git pull origin develop
+killall gunicorn
+
+./manage.py posts_chart >./static/js/posts_chart.js
+./manage.py comments_chart >./static/js/comments_chart.js
+
+rm -rf ../staticfiles
+
+./manage.py collectstatic --noinput
+
+nohup gunicorn deploy.wsgi:application --workers 4 --worker-class gevent &
+redis-cli -n 1 flushdb
