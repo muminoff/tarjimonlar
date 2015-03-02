d44e55ffb092d40b0aaae0ad242015d8945ddef6
Sardor <sardor@hanbiro.com>
2015-03-02 14:44:59 +0900

    Revert

--- config/common.py
+++ config/common.py
@@ -373,8 +373,7 @@ class Common(Configuration):
     }
     PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.yuglify.YuglifyCompressor'
     PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
-    # STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
-    STATICFILES_STORAGE = 'pipeline.storage.GZIPCachedStorage'
+    STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
     PIPELINE_VERSIONING = 'pipeline.versioning.hash.MD5Versioning'
     PIPELINE_ENABLED = True
     PIPELINE_AUTO = False
