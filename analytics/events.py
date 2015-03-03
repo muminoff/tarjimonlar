4e76c782abde70cfef44466d4ab489ce79d14b1a
Sardor <sardor@hanbiro.com>
2015-03-03 14:39:15 +0900

    Analytics

---
+++ analytics/events.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+from bitmapist import setup_redis, delete_all_events,\
+        mark_event, MonthEvents, WeekEvents,\
+        DayEvents, HourEvents, BitOpAnd, BitOpOr
+
+
+def user_loggedin_event(
