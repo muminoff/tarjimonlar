4ec426cc1ddc3626550968a37cb1e9a8603eee84
Sardor <smuminov@gmail.com>
2015-03-04 12:25:32 +0500

    Server side import

--- deploy/script/import.sh
+++ deploy/script/import.sh
@@ -2,14 +2,23 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
-echo 'Start' `date` >/tmp/last_time
-/home/ubuntu/tarjimonlar/manage.py import_members
-/home/ubuntu/tarjimonlar/manage.py import_posts
-/home/ubuntu/tarjimonlar/manage.py import_comments
-/home/ubuntu/tarjimonlar/manage.py update_index core
-/home/ubuntu/tarjimonlar/manage.py posts_chart >/home/ubuntu/tarjimonlar/static/js/posts_chart.js
-/home/ubuntu/tarjimonlar/manage.py comments_chart >/home/ubuntu/tarjimonlar/static/js/comments_chart.js
-rm -rf /home/ubuntu/staticfiles
-/home/ubuntu/tarjimonlar/manage.py collectstatic --noinput
-/usr/bin/redis-cli -n 1 flushdb
-echo 'Finish' `date` >>/tmp/last_time
+import_data_from_facebook() {
+	/home/ubuntu/tarjimonlar/manage.py import_members
+	/home/ubuntu/tarjimonlar/manage.py import_posts
+	/home/ubuntu/tarjimonlar/manage.py import_comments
+}
+update_search_index() {
+	/home/ubuntu/tarjimonlar/manage.py update_index core
+}
+build_assets() {
+	/home/ubuntu/tarjimonlar/manage.py posts_chart >/home/ubuntu/tarjimonlar/static/js/posts_chart.js
+	/home/ubuntu/tarjimonlar/manage.py comments_chart >/home/ubuntu/tarjimonlar/static/js/comments_chart.js
+	/home/ubuntu/tarjimonlar/manage.py collectstatic --noinput >/tmp/collecstatic`date`
+}
+clear_cache() {
+	echo "Cache clear..."
+	/usr/bin/redis-cli -n 1 flushdb
+}
+echo 'Start' `date +"%y%m%d%H%M"` >/tmp/last_time
+build_assets && clear_cache >>/tmp/last_time
+echo 'Finish' `date "%y%m%d%H%M"` >>/tmp/last_time
