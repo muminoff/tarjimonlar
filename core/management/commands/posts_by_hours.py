828a18764f81d649978ed1c866a623bb19dcdbfa
Sardor <sardor@hanbiro.com>
2015-03-13 17:55:59 +0900

    Daytime

--- core/management/commands/posts_by_hours.py
+++ core/management/commands/posts_by_hours.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from core.models import Member, Post, Comment
-from django.db.models import Count, Max, Min, Avg
+from django.db.models import Count, Max, Min, Avg, Sum
 from django.template.loader import render_to_string
 from django.shortcuts import render_to_response
 from django.template import Context, loader
@@ -15,5 +15,13 @@ class Command(BaseCommand):
         posts_by_hours = Post.objects.extra({
             "hour": "extract('hour' from created_time at time zone 'UZT')"
             }).values("hour").order_by("hour").annotate(num_posts=Count("id"))
+
+        posts = []
         for post in posts_by_hours:
-            print post["hour"], post["num_posts"]
+            pobj = {}
+            pobj["daytime"] = "morning" if post["hour"] < 12 and post["hour"] > 0 else "afternoon"
+            pobj["hour"] = post["hour"]
+            pobj["num_posts"] = post["num_posts"]
+            posts.append(pobj)
+
+        print posts
