c71a2ab10132c28e0df55de45aa360fd0a4e1281
Sardor <sardor@hanbiro.com>
2015-05-08 14:29:32 +0900

    feed

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,13 +6,13 @@ from django.utils.translation import ugettext_lazy as _
 
 def navbar(request):
     menus = OrderedDict([
+        ('feed_page', {'title': 'Сўнгги янгиликлар', 'icon': 'article', 'submenu': False}),
         ('stats_page', {'title': 'Умумий статистика', 'icon': 'stat', 'submenu': False}),
         ('members_page', {'title': 'Иштирокчилар', 'icon': 'user', 'submenu': False}),
         ('posts_page', {'title': 'Постлар', 'icon': 'post', 'submenu': False}),
         ('comments_page', {'title': 'Шарҳлар', 'icon': 'comment', 'submenu': False}),
         ('search_page', {'title': 'Қидирув', 'icon': 'search', 'submenu': False}),
         ('about_page', {'title': 'Лойиҳа ҳақида', 'icon': 'info', 'submenu': False}),
-        ('feed_page', {'title': 'News', 'icon': 'book', 'submenu': False}),
         ])
 
     try: 
