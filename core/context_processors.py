30a894fb9669a18a5738e35fa03a69fb2bc29253
Sardor <sardor@hanbiro.com>
2015-05-09 18:30:18 +0900

    context

--- core/context_processors.py
+++ core/context_processors.py
@@ -13,6 +13,7 @@ def navbar(request):
         ('comments_page', {'title': 'Шарҳлар', 'icon': 'comment', 'submenu': False}),
         ('search_page', {'title': 'Қидирув', 'icon': 'search', 'submenu': False}),
         ('about_page', {'title': 'Лойиҳа ҳақида', 'icon': 'info', 'submenu': False}),
+        ('subcribe_page', {'title': 'Обуна', 'icon': 'broadcast', 'submenu': False}),
         ])
 
     try: 
