6423b7aa8af047d875ea6db5a269be1763782ab5
Sardor <smuminov@gmail.com>
2015-05-09 11:34:59 +0500

    default

--- config/common.py
+++ config/common.py
@@ -399,10 +399,12 @@ class Common(Configuration):
 
     # Haystack
     HAYSTACK_CONNECTIONS = {
+        'default': {
             'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
             'URL': 'http://127.0.0.1:9200/',
             'INDEX_NAME': 'tarjimonlar',
-            }
+        },
+    }
     HAYSTACK_DEFAULT_OPERATOR = 'OR'
     HAYSTACK_SEARCH_RESULTS_PER_PAGE = 10
     # HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
