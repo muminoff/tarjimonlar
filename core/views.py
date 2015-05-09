5b2b2a7c52e3c178e807ffd76013098e81d7410b
Sardor <sardor@hanbiro.com>
2015-05-09 11:52:20 +0900

    2015 posts

--- core/views.py
+++ core/views.py
@@ -103,7 +103,7 @@ def members_page(request):
 # @login_required
 # @cache_page(60 * 5)
 def posts_page(request):
-    posts_by_months = Post.objects.filter(created_time__year=2014).extra({
+    posts_by_months = Post.objects.filter(created_time__year=2015).extra({
         "month": "EXTRACT('month' FROM created_time AT TIME ZONE 'UZT')"
         }).values("month").order_by("month").annotate(num_posts=Count("id"))
     context = {
